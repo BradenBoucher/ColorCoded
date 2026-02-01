91d4ce704f3858611b60a9e7308ccb3cb7ee237d
Braden Boucher <b1boucher@student.bridgew.edu>
2026-01-31 21:37:50 -0500

    Initial Commit

---
+++ ColorCoded/ColorCodedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCodedApp.swift
+//  ColorCoded
+//
+//  Created by Braden on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct ColorCodedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
